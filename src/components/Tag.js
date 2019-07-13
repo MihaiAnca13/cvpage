1d79b8f93694670be42a1a42e406d9fb75e13e2e
Mihai <mihai.anca13@gmail.com>
2019-07-13 14:52:09 +0100

    Initial code base

--- src/components/Tag.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Link from 'gatsby-link'
-
-const Tag = ( { tag } ) =>
-    <Link className="tag-link" to={ `tags/${ tag }` }>
-        { tag }
-    </Link>
-
-export default Tag
